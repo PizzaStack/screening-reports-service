f32782cfd33c6a64069e7b206519a22e588399c4
John Masiello <johnmasiello@github.com>
2019-02-07 17:15:56 -0600

    Added a github README.md documentation for util/Time

--- src/main/java/com/revature/screenforce/util/Time.java
+++ src/main/java/com/revature/screenforce/util/Time.java
@@ -7,14 +7,34 @@ import java.time.LocalDate;
  **/
 // http://tutorials.jenkov.com/java-date-time/localdate.html
 public class Time {
+	/**
+	 * 
+	 * @param from
+	 * 	The end date of the period for reporting aggregate metrics
+	 * @param numWeeks The number of weeks
+	 * @return The start date
+	 */
 	public LocalDate getWeekToDate(LocalDate from, int numWeeks) {
 		return from.minusWeeks(numWeeks);
 	}
 	
+	/**
+	 * 
+	 * @param from
+	 * 	The end date of the period for reporting aggregate metrics
+	 * @param numMonths The number of months
+	 * @return The start date
+	 */
 	public LocalDate getMonthToDate(LocalDate from, int numMonths) {
 		return from.minusMonths(numMonths);
 	}
 	
+	/**
+	 * 
+	 * @param from
+	 * 	The end date of the period for reporting aggregate metrics
+	 * @return The start date one year prior to from
+	 */
 	public LocalDate getYearToDate(LocalDate from) {
 		return from.minusYears(1);
 	}
