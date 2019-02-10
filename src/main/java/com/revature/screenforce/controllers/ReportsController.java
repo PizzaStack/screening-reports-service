22d926812b5e12c8d68e8db372d51be1a45a7297
Boyd Thurston <boydthurston7@gmail.com>
2019-02-09 19:57:57 -0600

    Added email service

--- src/main/java/com/revature/screenforce/controllers/ReportsController.java
+++ src/main/java/com/revature/screenforce/controllers/ReportsController.java
@@ -1,15 +1,30 @@
 package com.revature.screenforce.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.screenforce.services.ReportsServiceImp;
 
 @RestController
 @CrossOrigin
 @RequestMapping(value="/reports")
 public class ReportsController {
+	@Autowired
+	ReportsServiceImp reportsServiceImp;
+	
+	@GetMapping(value="/email", produces= MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<String> getAllEmails(@RequestParam(value = "email") String email){
+		List<String> emails = this.reportsServiceImp.getAllEmails(email);
+		return emails;
+	}
+	
 	
 }
---
+++ src/main/java/com/revature/screenforce/daos/ScreenerRepository.java
@@ -0,0 +1,15 @@
+package com.revature.screenforce.daos;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.screenforce.beans.Screener;
+
+@Repository
+public interface ScreenerRepository extends JpaRepository<Screener, Integer>{
+    public Screener getByEmail(String email);
+
+	public List<Screener> findAllByEmailContainingIgnoreCase(String email);
+}
